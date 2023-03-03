10793fde7d5efa9015c215fa1f5b54a538cef5fe
ImK4Lok <samkalok107@gmail.com>
2023-03-03 17:46:33 +0800

    update events w/ fetch in js

--- script.js
+++ script.js
@@ -8,18 +8,22 @@ fetch("https://backend.cpsumsu.org/api/events")
     .then(function(result) {
         console.log(result.data)
         events = result.data
-        // createEventCards(events)
+        createEventCards(events)
     })
 
 function createEventCards(items) {
     let events = '';
     
     items.forEach(function(item) {
-        events += ` <div>
-                        <span>${item.event_type}</span>
-                        <img src="${item.image}" alt="Post for ${item.name}">
-                        <p>${item.date}</p>
-                        <h3>${item.name}</h3>
+        events += ` <div class="event">
+                        <div class="image-container">
+                            <span class="type">${item.event_type}</span>
+                            <img src="${item.image}" alt="Post for ${item.name}">
+                        </div>
+                        <div class="event-info">
+                            <p class="date">${item.date}</p>
+                            <h3>${item.name}</h3>
+                        </div>
                     </div>`
     })
 
