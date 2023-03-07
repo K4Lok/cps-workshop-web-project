4c1c1be757b416d65362a19f3964af32302fd257
K4Lok <samkalok107@gmail.com>
2023-03-07 13:04:03 +0800

    update events are clickable to CPS event page

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const container = document.getElementById('events');
 
 let events = [];
 fetch("https://backend.cpsumsu.org/api/events")
-    .then(function(res) {
+    .then(function (res) {
         return res.json()
     })
-    .then(function(result) {
+    .then(function (result) {
         console.log(result.data)
         events = result.data
         createEventCards(events)
@@ -13,17 +13,22 @@ fetch("https://backend.cpsumsu.org/api/events")
 
 function createEventCards(items) {
     let events = '';
-    
-    items.forEach(function(item) {
+    const CPS_URL = 'https://cps-website-eight.vercel.app/events';
+
+    items.forEach(function (item) {
+        const url = `${CPS_URL}/${item.id}`;
+
         events += ` <div class="event">
-                        <div class="image-container">
-                            <span class="type">${item.event_type}</span>
-                            <img src="${item.image}" alt="Post for ${item.name}">
-                        </div>
-                        <div class="event-info">
-                            <p class="date">${item.date}</p>
-                            <h3>${item.name}</h3>
-                        </div>
+                        <a target="_blank" href=${url}>
+                            <div class="image-container">
+                                <span class="type">${item.event_type}</span>
+                                <img src="${item.image}" alt="Post for ${item.name}">
+                            </div>
+                            <div class="event-info">
+                                <p class="date">${item.date}</p>
+                                <h3>${item.name}</h3>
+                            </div>
+                        </a>
                     </div>`
     })
 
